b61dd53f437754c3cb5ca55a91517252eb1f4c80
CommanderRedYT <ccommanderred@gmail.com>
2022-01-18 09:15:35 +0100

    Moved FEATURE_DNS_NS into config-system

--- main/newsettings.h
+++ main/newsettings.h
@@ -306,6 +306,7 @@ public:
         ConfigWrapper<bool> garage                {false,                                  DoReset,   {},                         "f_garage"            };
         ConfigWrapper<bool> cloud                 {false,                                  DoReset,   {},                         "f_cloud"             };
         ConfigWrapper<bool> udpcloud              {false,                                  DoReset,   {},                         "f_udpcloud"          };
+        ConfigWrapper<bool> dnsannounce           {false,                                  DoReset,   {},                         "f_dnsannounce"       };
         ConfigWrapper<bool> gschissene_diode      {false,                                  DoReset,   {},                         "f_gschissDiode"      };
     } feature;
 
@@ -599,7 +600,8 @@ public:
     x(feature.webserver_disable_lock) \
     x(feature.garage) \
     x(feature.udpcloud) \
-    x(feature.cloud)
+    x(feature.cloud) \
+    x(feature.dnsannounce)
     //x(bleSettings.bleEnabled)
 
 #define FEATURES(x) \
@@ -607,7 +609,8 @@ public:
     x(feature.webserver_disable_lock) \
     x(feature.garage) \
     x(feature.udpcloud) \
-    x(feature.cloud)
+    x(feature.cloud) \
+    x(feature.dnsannounce)
     //x(feature.gschisseneDiode)
 
     template<typename T>
