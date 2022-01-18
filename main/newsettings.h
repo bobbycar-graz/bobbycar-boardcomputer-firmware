585e7cdd64bdea055bf3747e9c2fad7d305883cc
CommanderRedYT <ccommanderred@gmail.com>
2022-01-18 11:37:53 +0100

    Moved FEATURE_BLE into config-system

--- main/newsettings.h
+++ main/newsettings.h
@@ -308,6 +308,7 @@ public:
         ConfigWrapper<bool> udpcloud              {false,                                  DoReset,   {},                         "f_udpcloud"          };
         ConfigWrapper<bool> dnsannounce           {false,                                  DoReset,   {},                         "f_dnsannounce"       };
         ConfigWrapper<bool> ntp                   {false,                                  DoReset,   {},                         "f_ntp"               };
+        ConfigWrapper<bool> ble                   {false,                                  DoReset,   {},                         "f_ble"               };
         ConfigWrapper<bool> gschissene_diode      {false,                                  DoReset,   {},                         "f_gschissDiode"      };
     } feature;
 
@@ -596,14 +597,15 @@ public:
     x(espnow.syncTimeWithOthers) \
     x(espnow.syncBlink) \
     \
-    x(feature.gschissene_diode) \
     x(feature.ledstrip) \
     x(feature.webserver_disable_lock) \
     x(feature.garage) \
     x(feature.udpcloud) \
     x(feature.cloud) \
     x(feature.dnsannounce) \
-    x(feature.ntp)
+    x(feature.ntp) \
+    x(feature.ble) \
+    x(feature.gschissene_diode)
     //x(bleSettings.bleEnabled)
 
 #define FEATURES(x) \
@@ -613,7 +615,8 @@ public:
     x(feature.udpcloud) \
     x(feature.cloud) \
     x(feature.dnsannounce) \
-    x(feature.ntp)
+    x(feature.ntp) \
+    x(feature.ble)
     //x(feature.gschisseneDiode)
 
     template<typename T>
